3352812aa709f52261a34d3d4ba7cb768a40d7bf
prashant <prashanttivhale@gmail.com>
2021-01-27 17:51:16 +0530

    second commit

--- src/main/java/com/appoint/service/PdfGenerator.java
+++ src/main/java/com/appoint/service/PdfGenerator.java
@@ -26,11 +26,11 @@ public class PdfGenerator {
 		try {
 			
 			
-	        OutputStream file = new FileOutputStream(new File("/home/aby/Documents/javapdf/d.pdf"));
+	        OutputStream file = new FileOutputStream(new File("/home/aby/Documents/javapdf/appointment.pdf"));
 	        Document document = new Document();
 	        PdfWriter.getInstance(document, file);
 	        
-	      //Inserting Table in PDF
+	      
 		     PdfPTable table=new PdfPTable(8);
 
 	                PdfPCell cell = new PdfPCell (new Paragraph ("Appointment Registration"));
@@ -62,7 +62,6 @@ public class PdfGenerator {
 			    	  table.addCell(row.getName());
 			    	  table.addCell(String.valueOf(row.getAge())); 
 			    	  table.addCell(row.getDob());
-			    	  
 			    	  table.addCell(row.getBloodGroup());
 			    	  table.addCell(row.getMobile());
 			    	  table.addCell(row.getEmail());
@@ -75,44 +74,14 @@ public class PdfGenerator {
 	             
 			      
 			      
-			      table.setSpacingBefore(30.0f);       // Space Before table starts, like margin-top in CSS
-			      table.setSpacingAfter(30.0f);        // Space After table starts, like margin-Bottom in CSS								          
+			      table.setSpacingBefore(30.0f);        
+			      table.setSpacingAfter(30.0f);         							          
 
-					/*
-					 * //Inserting List in PDF List list=new List(true,30); list.add(new
-					 * ListItem("Java4s")); list.add(new ListItem("Php4s")); list.add(new
-					 * ListItem("Some Thing..."));
-					 */
-
-					/*
-					 * //Text formating in PDF Chunk chunk=new
-					 * Chunk("Welecome To Java4s Programming Blog...");
-					 * chunk.setUnderline(+1f,-2f);//1st co-ordinate is for line width,2nd is space
-					 * between Chunk chunk1=new Chunk("Php4s.com"); chunk1.setUnderline(+4f,-8f);
-					 * chunk1.setBackground(new BaseColor (17, 46, 193));
-					 */    
-
-		 //Now Insert Every Thing Into PDF Document
-	        document.open();//PDF document opened........			       
+					 
+	        document.open(); 			       
 
 	        document.add(table);
-				//document.add(image);
-	//////
-				/*
-				 * document.add(Chunk.NEWLINE); //Something like in HTML
-				 * 
-				 * document.add(new Paragraph("Dear Java4s.com")); document.add(new
-				 * Paragraph("Document Generated On - "+new Date().toString()));
-				 * 
-				 * document.add(table);
-				 * 
-				 * document.add(chunk); document.add(chunk1);
-				 * 
-				 * document.add(Chunk.NEWLINE); //Something like in HTML
-				 * 
-				 * document.newPage(); //Opened new page document.add(list); //In the new page
-				 * we are going to add list
-				 */
+			
 	        document.close();
 
 		             file.close();
